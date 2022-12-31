c4953e8071796ff2cd21ffe671cb9a31e1341fba
Yaroslav Prokhorov <y.prokhorov@gardatech.ru>
2022-12-31 13:44:24 +0300

    add "github.com/apex/gateway" to handle with AWS.

--- go.mod
+++ go.mod
@@ -1,3 +1,17 @@
 module SimpleServer
 
 go 1.19
+
+require (
+	github.com/PuerkitoBio/goquery v1.5.1 // indirect
+	github.com/andybalholm/cascadia v1.2.0 // indirect
+	github.com/apex/gateway v1.1.2 // indirect
+	github.com/aws/aws-lambda-go v1.17.0 // indirect
+	github.com/carlmjohnson/feed2json v0.20.1 // indirect
+	github.com/gorilla/feeds v1.1.1 // indirect
+	github.com/mmcdole/gofeed v1.0.0 // indirect
+	github.com/mmcdole/goxpp v0.0.0-20181012175147-0068e33feabf // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/net v0.0.0-20200707034311-ab3426394381 // indirect
+	golang.org/x/text v0.3.3 // indirect
+)
--- main.go
+++ main.go
@@ -1,16 +1,25 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"github.com/apex/gateway"
 	"io"
+	"log"
 	"net/http"
 )
 
 func main() {
-	var port string
-	port = "80"
-
-	http.HandleFunc("/", func(w http.ResponseWriter, request *http.Request) {
-		io.WriteString(w, "Hello, world!\n")
-	})
-	http.ListenAndServe(":"+port, nil)
+	port := flag.Int("port", -1, "specify a port to use http rather than AWS Lambda")
+	flag.Parse()
+	listener := gateway.ListenAndServe
+	portStr := "n/a"
+	if *port != -1 {
+		portStr = fmt.Sprintf(":%d", *port)
+		listener = http.ListenAndServe
+		http.HandleFunc("/", func(w http.ResponseWriter, request *http.Request) {
+			io.WriteString(w, "Hello, world!\n")
+		})
+	}
+	log.Fatal(listener(portStr, nil))
 }
